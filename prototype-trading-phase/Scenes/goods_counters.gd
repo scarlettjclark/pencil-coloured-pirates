d47a6659dd97e7a5771d176cb36a595ea22e2841
moehike <59832236+scarlettjclark@users.noreply.github.com>
2025-12-27 09:59:53 +0000

    reimplemented labels using methods
    Using Godot's inbuilt setters and getters meant that the integers being set happened before the counters were, causing a null reference error

--- prototype-trading-phase/Scenes/goods_counters.gd
+++ prototype-trading-phase/Scenes/goods_counters.gd
@@ -9,34 +9,13 @@ extends VBoxContainer
 @export var spices_counter : GoodCounter
 @export var gold_counter : GoodCounter
 @export var notoriety_counter : GoodCounter
-@export var crew : int:
-	set(new_crew):
-		crew_counter.set_value(new_crew)
-		crew = new_crew
-@export var munitions : int:
-	set(new_munitions):
-		munitions_counter.set_value(new_munitions)
-		munitions = new_munitions
-@export var supplies : int:
-	set(new_supplies):
-		supplies_counter.set_value(new_supplies)
-		supplies = new_supplies
-@export var booze : int:
-	set(new_booze):
-		booze_counter.set_value(new_booze)
-		booze = new_booze
-@export var cloth : int:
-	set(new_cloth):
-		cloth_counter.set_value(new_cloth)
-		cloth = new_cloth
-@export var spices : int:
-	set(new_spices):
-		spices_counter.set_value(new_spices)
-		spices = new_spices
-@export var gold : int:
-	set(new_gold):
-		gold_counter.set_value(new_gold)
-		gold = new_gold
+@export var crew : int
+@export var munitions : int
+@export var supplies : int
+@export var booze : int
+@export var cloth : int
+@export var spices : int
+@export var gold : int
 		
 @export var notoriety : int:
 	set(new_notoriety):
@@ -45,7 +24,7 @@ extends VBoxContainer
 
 func _ready():
 	set_label_initials()
-	reset_counters()
+	display_goods()
 
 func set_label_initials() -> void:
 	crew_counter.set_label('c')
@@ -57,38 +36,56 @@ func set_label_initials() -> void:
 	gold_counter.set_label('G')
 	notoriety_counter.set_label('N')
 
-func reset_counters() -> void:
-	crew = 0
-	munitions = 0
-	supplies = 0
-	booze = 0
-	cloth = 0
-	spices = 0
-	gold = 0
-	notoriety = 0
+func display_goods() -> void:
+	crew_counter.set_value(crew)
+	munitions_counter.set_value(munitions)
+	supplies_counter.set_value(supplies)
+	booze_counter.set_value(booze)
+	cloth_counter.set_value(cloth)
+	spices_counter.set_value(spices)
+	gold_counter.set_value(gold)
+	notoriety_counter.set_value(notoriety)
+
+#func reset_counters() -> void:
+	#crew = 0
+	#munitions = 0
+	#supplies = 0
+	#booze = 0
+	#cloth = 0
+	#spices = 0
+	#gold = 0
+	#notoriety = 0
 
 func set_crew(new_crew : int) -> void:
 	crew_counter.set_value(new_crew)
+	crew = new_crew
 
 func set_munitions(new_munitions : int) -> void:
 	munitions_counter.set_value(new_munitions)
+	munitions = new_munitions
 
 func set_supplies(new_supplies : int) -> void:
 	supplies_counter.set_value(new_supplies)
+	supplies = new_supplies
 
 func set_booze(new_booze : int) -> void:
 	booze_counter.set_value(new_booze)
+	booze = new_booze
 
 func set_cloth(new_cloth : int) -> void:
 	cloth_counter.set_value(new_cloth)
+	cloth = new_cloth
 
 func set_spices(new_spices : int) -> void:
 	spices_counter.set_value(new_spices)
+	spices = new_spices
 
 func set_gold(new_gold : int) -> void:
 	gold_counter.set_value(new_gold)
+	gold = new_gold
 
 func set_notoriety(new_notoriety : int) -> void:
 	notoriety_counter.set_value(new_notoriety)
+	notoriety = new_notoriety
 	
 	
