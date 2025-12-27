ede9a574ead2392efb5f13582451a7dbf9463a7d
moehike <59832236+scarlettjclark@users.noreply.github.com>
2025-12-27 10:07:26 +0000

    refactored to use goods code

--- prototype-trading-phase/Scenes/goods_counters.gd
+++ prototype-trading-phase/Scenes/goods_counters.gd
@@ -9,18 +9,7 @@ extends VBoxContainer
 @export var spices_counter : GoodCounter
 @export var gold_counter : GoodCounter
 @export var notoriety_counter : GoodCounter
-@export var crew : int
-@export var munitions : int
-@export var supplies : int
-@export var booze : int
-@export var cloth : int
-@export var spices : int
-@export var gold : int
-		
-@export var notoriety : int:
-	set(new_notoriety):
-		notoriety_counter.set_value(new_notoriety)
-		notoriety = new_notoriety
+@export var store : Goods
 
 func _ready():
 	set_label_initials()
@@ -37,55 +26,55 @@ func set_label_initials() -> void:
 	notoriety_counter.set_label('N')
 
 func display_goods() -> void:
-	crew_counter.set_value(crew)
-	munitions_counter.set_value(munitions)
-	supplies_counter.set_value(supplies)
-	booze_counter.set_value(booze)
-	cloth_counter.set_value(cloth)
-	spices_counter.set_value(spices)
-	gold_counter.set_value(gold)
-	notoriety_counter.set_value(notoriety)
+	crew_counter.set_value(store.crew)
+	munitions_counter.set_value(store.munitions)
+	supplies_counter.set_value(store.supplies)
+	booze_counter.set_value(store.booze)
+	cloth_counter.set_value(store.cloth)
+	spices_counter.set_value(store.spices)
+	gold_counter.set_value(store.gold)
+	notoriety_counter.set_value(store.notoriety)
 
 #func reset_counters() -> void:
-	#crew = 0
-	#munitions = 0
-	#supplies = 0
-	#booze = 0
-	#cloth = 0
-	#spices = 0
-	#gold = 0
-	#notoriety = 0
+	#store.crew = 0
+	#store.munitions = 0
+	#store.supplies = 0
+	#store.booze = 0
+	#store.cloth = 0
+	#store.spices = 0
+	#store.gold = 0
+	#store.notoriety = 0
 
 func set_crew(new_crew : int) -> void:
 	crew_counter.set_value(new_crew)
-	crew = new_crew
+	store.crew = new_crew
 
 func set_munitions(new_munitions : int) -> void:
 	munitions_counter.set_value(new_munitions)
-	munitions = new_munitions
+	store.munitions = new_munitions
 
 func set_supplies(new_supplies : int) -> void:
 	supplies_counter.set_value(new_supplies)
-	supplies = new_supplies
+	store.supplies = new_supplies
 
 func set_booze(new_booze : int) -> void:
 	booze_counter.set_value(new_booze)
-	booze = new_booze
+	store.booze = new_booze
 
 func set_cloth(new_cloth : int) -> void:
 	cloth_counter.set_value(new_cloth)
-	cloth = new_cloth
+	store.cloth = new_cloth
 
 func set_spices(new_spices : int) -> void:
 	spices_counter.set_value(new_spices)
-	spices = new_spices
+	store.spices = new_spices
 
 func set_gold(new_gold : int) -> void:
 	gold_counter.set_value(new_gold)
-	gold = new_gold
+	store.gold = new_gold
 
 func set_notoriety(new_notoriety : int) -> void:
 	notoriety_counter.set_value(new_notoriety)
-	notoriety = new_notoriety
+	store.notoriety = new_notoriety
 	
 	
